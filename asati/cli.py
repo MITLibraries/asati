14500b84999c9df1bb985c813f0caf1a51f66021
Eric Hanson <ehanson@mit.edu>
2024-12-02 13:18:11 -0500

    Add functionality to CLI (#6)
    * Add functionality to CLI
    
    Why these changes are being introduced:
    * Given the application's simplicity, the majority of the application's functionality will reside in the CLI.
    
    How this addresses that need:
    * Update cli.py with the application's functionality
    * Update dependencies
    * Update README.md
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1093
    
    * Remove pinned pydantic version after bug fix
    
    * Updates based on discussion in PR #6
    
    * Add comments to `cli.main` for readability
    * Reorder `cli.main` for readability
    * Update logging in SSMClient methods

--- asati/cli.py
+++ asati/cli.py
@@ -1,10 +1,14 @@
 import logging
+import os
 from datetime import timedelta
 from time import perf_counter
 
 import click
+import pyairtable
+from asnake.client import ASnakeClient  # type: ignore[import-untyped]
 
 from asati.config import configure_logger, configure_sentry
+from asati.utils import SSMClient, parse_accession_number, parse_extent_data
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +24,67 @@ def main(*, verbose: bool) -> None:
     logger.info(configure_sentry())
     logger.info("Running process")
 
-    # Do things here!
+    # Get last accession ID from SSM parameter
+    ssm_client = SSMClient()
+    last_accession_parameter = os.environ["LAST_ACCESSION_PARAMETER"]
+    last_accession_uri_id = ssm_client.get_parameter(last_accession_parameter)
+
+    # Retrieve list of all accessions from ArchivesSpace
+    asnake_client = ASnakeClient(
+        baseurl=os.environ["ARCHIVESSPACE_URL"],
+        username=os.environ["ARCHIVESSPACE_USER"],
+        password=os.environ["ARCHIVESSPACE_PASSWORD"],
+    )
+    accession_uri_ids = asnake_client.get(
+        "/repositories/2/accessions?all_ids=true"
+    ).json()
+
+    # Determine new accessions to add to Airtable, exit early if none
+    accessions_to_add = sorted(
+        [
+            accession_uri_id
+            for accession_uri_id in accession_uri_ids
+            if accession_uri_id > int(last_accession_uri_id)
+        ]
+    )
+    if not accessions_to_add:
+        logger.info("No new accessions to add to Airtable.")
+        return
+    logger.info(f"Adding the following accessions to Airtable: {accessions_to_add}")
+
+    # Establish connection to Airtable
+    airtable_api = pyairtable.Api(os.environ["AIRTABLE_TOKEN"])
+    airtable_table = airtable_api.table(
+        os.environ["AIRTABLE_BASE_ID"], os.environ["AIRTABLE_TABLE_NAME"]
+    )
+    logger.info(
+        f"Airtable client configured for base '{airtable_table.base}'"
+        f" and table '{airtable_table.name}'"
+    )
+
+    # Add accessions to Airtable
+    for accession_uri_id in accessions_to_add:
+        accession_uri = f"/repositories/2/accessions/{accession_uri_id}"
+        accession_record = asnake_client.get(accession_uri).json()
+        logger.debug(f"Retrieved record: {accession_uri}")
+        accession_data = {
+            "Accession Title": accession_record["title"],
+            "Accession Number": parse_accession_number(accession_record),
+            "Current Status": "Unassigned",
+            **parse_extent_data(accession_record),
+        }
+        logger.debug(f"Data extracted from ArchivesSpace: {accession_data}")
+
+        # Post new rows to Airtable
+        response = airtable_table.create(accession_data)
+        logger.info(
+            "Airtable row created for Accession Number: "
+            f"'{response["fields"]["Accession Number"]}'"
+        )
+
+        # Update SSM parameter
+        logger.debug(f"Last accession ID processed: '{accession_uri_id}'")
+        ssm_client.update_parameter(last_accession_parameter, str(accession_uri_id))
 
     elapsed_time = perf_counter() - start_time
     logger.info(
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,47 @@
+import logging
+from unittest.mock import patch
+
 from asati.cli import main
 
 
-def test_cli_no_options(caplog, runner):
-    result = runner.invoke(main)
-    assert result.exit_code == 0
-    assert "Logger 'root' configured with level=INFO" in caplog.text
-    assert "Running process" in caplog.text
-    assert "Total time to complete process" in caplog.text
-
-
-def test_cli_all_options(caplog, runner):
+@patch("asati.cli.ASnakeClient")
+@patch("asati.cli.pyairtable")
+def test_cli_new_accessions_success(
+    mocked_airtable, mocked_asnake, aspace_record, caplog, runner
+):
+    mocked_asnake.return_value.get.return_value.json.side_effect = [
+        [1234, 5678],
+        aspace_record,
+    ]
+    mocked_airtable.Api.return_value.table.return_value.create.return_value = {
+        "fields": {"Accession Number": "2025-014"}
+    }
+    caplog.set_level(logging.DEBUG)
     result = runner.invoke(main, ["--verbose"])
     assert result.exit_code == 0
     assert "Logger 'root' configured with level=DEBUG" in caplog.text
     assert "Running process" in caplog.text
+    assert (
+        "Data extracted from ArchivesSpace: {'Accession Title': 'MIT Libraries records "
+        "and Bibliotech newsletters 2024 July transfer', 'Accession Number': '2025-014',"
+        " 'Current Status': 'Unassigned', 'Extent Number': 1.0, 'Extent Type': 'box(es)'}"
+        in caplog.text
+    )
+    assert "Airtable row created for Accession Number: '2025-014'" in caplog.text
+    assert (
+        "Updated SSM parameter '/apps/asati/last-accession-uri-id' to '5678'"
+        in caplog.text
+    )
     assert "Total time to complete process" in caplog.text
+
+
+@patch("asati.cli.ASnakeClient")
+def test_cli_no_accessions_success(mocked_asnake, caplog, runner):
+    mocked_asnake.return_value.get.return_value.json.return_value = [1234]
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    assert "Logger 'root' configured with level=INFO" in caplog.text
+    assert "Running process" in caplog.text
+    assert "No new accessions to add to Airtable." in caplog.text
+    assert "Airtable row created for Accession Number:" not in caplog.text
+    assert "Updated SSM parameter" not in caplog.text
